07a26d2223b8144ef11213d448ff9380b4f4c8d1
Duffeck <lsro@gft.com>
2021-02-06 14:59:11 -0300

    um tapa na gostosa

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Yeas baby</h2>
+          <h2>Yes baby</h2>
         </div>
         <p className="App-intro">
           Thank you
